74ad38efddfcc57f05740cec730be863d4bcfa3b
WaqasSiddique02 <siddique02mwaqas@gmail.com>
2024-07-14 14:34:47 +0500

    Added reading from DB

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mongoose = require("mongoose");
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
+let posts=[];
 
 const app = express();
 
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect('mongodb://127.0.0.1:27017/blogDB');
+mongoose.connect('mongodb://localhost:27017/blogDB');
 
 const blogSchema= new mongoose.Schema({
   name : String,
@@ -26,19 +26,18 @@ const blogSchema= new mongoose.Schema({
 
 const Blog = mongoose.model('Blog',blogSchema);
 
-app.get("/",function(req,res){
-  Blog.find({},function(err,posts){
-    res.render("home",
-      {
-        homeStartingContent:homeStartingContent,
-        posts:posts
+app.get("/", function(req, res){
+  Blog.find({})
+    .then(function(posts){
+      res.render("home", {
+        homeContent: homeStartingContent,
+        posts: posts
       });
-  });
-
-  // res.render("home.ejs",{
-  //   homeContent:homeStartingContent,
-  //   posts:posts,
-  // });
+    })
+    .catch(function(err){
+      console.log(err);
+      res.status(500).send("Error fetching posts");
+    });
 });
 
 app.get("/about",function(req,res){
@@ -55,35 +54,39 @@ app.get("/compose",function(req,res){
 });
 
 
-
-
 app.post("/compose",function(req,res){
-
   const post = new Blog({
     name: req.body.postTitle,
     content: req.body.postContent
   });
 
-  post.save();
+  post.save()
+  .then(function(){
+    res.redirect("/");
+  })
+  .catch(function(err){
+    console.log(err);
+  });
 
-  res.redirect("/");
 });
 
-app.get("/posts/:postName",function(req,res){
-  const postTit=_.lowerCase(req.params.postName);
- 
-  posts.forEach(function(posts){ 
-    var storedTitle=_.lowerCase(posts.title);
-    if(storedTitle===postTit){
-      res.render("post",{
-        title:posts.title,
-        body:posts.content
+app.get("/posts/:postId", function(req, res){
+  const requestedPostId = req.params.postId;
+
+  Blog.findOne({_id: requestedPostId})
+    .then(function(post){
+      res.render("post", {
+        title: post.name,
+        content: post.content
       });
-    }
-    
-});
+    })
+    .catch(function(err){
+      console.log(err);
+      res.status(404).send("Post not found");
+    });
 });
 
+  
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
